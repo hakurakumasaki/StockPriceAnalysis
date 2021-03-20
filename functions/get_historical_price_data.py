7a1e3fa0547472ea903bc5bac715cf528a0ffdf6
Masaki <hakurakumasaki@gmail.com>
2021-03-20 17:55:02 +0900

    add get_historical_price_data

--- functions/get_historical_price_data.py
+++ functions/get_historical_price_data.py
@@ -1,36 +1,51 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import chromedriver_binary
 import time
-import csv
 import os
 import pandas as pd
 import numpy as np
 
-# get yahoo login informaiton
-with open('../../login_info/yahoo.csv') as f:
-    user_name_yahoo, login_password_yahoo = f.read().split(",")
+
+def init_driver():
+    download_directory_path = os.path.abspath(os.getcwd() + "/../../download_tmp/")
+    options_chrome = webdriver.ChromeOptions()
+    prefs = {"download.default_directory": download_directory_path}
+    options_chrome.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(options=options_chrome)
+    return driver
+
 
 # login to yahoo
-browser = webdriver.Chrome()
-url_login = "https://login.yahoo.co.jp/config/login"
-browser.get(url_login)
-browser.find_element_by_id('username').send_keys(user_name_yahoo)
-time.sleep(2)
-browser.find_element_by_id('btnNext').click()
-time.sleep(2)
-browser.find_element_by_id('passwd').send_keys(login_password_yahoo)
-time.sleep(2)
-browser.find_element_by_id('btnSubmit').click()
-time.sleep(3)
+def login_yahoo(driver):
+    with open('../../login_info/yahoo.csv') as f:
+        user_name_yahoo, login_password_yahoo = f.read().split(",")
+    url_login = "https://login.yahoo.co.jp/config/login"
+    driver.get(url_login)
+    driver.find_element_by_id('username').send_keys(user_name_yahoo)
+    time.sleep(2)
+    driver.find_element_by_id('btnNext').click()
+    time.sleep(2)
+    driver.find_element_by_id('passwd').send_keys(login_password_yahoo)
+    time.sleep(2)
+    driver.find_element_by_id('btnSubmit').click()
+    time.sleep(3)
 
-# download file from yahoo
-ID_list = list(pd.read_csv('../list/stock_list.csv')['code'].astype(str).map(str) + ('.T'))
-print(ID_list)
 
-for ID in ID_list:
-    url_csvfile = "https://download.finance.yahoo.co.jp/common/history/{security_code}.csv".format(security_code=ID)
-    print(url_csvfile)
-    browser.get(url_csvfile)
-    time.sleep(np.random.rand()*10)
+def get_data(driver):
+    id_list = list(pd.read_csv('../list/stock_list.csv')['code'].astype(str).map(str) + ('.T'))
+    for ID in id_list[:10]:
+        url_csvfile = "https://download.finance.yahoo.co.jp/common/history/{security_code}.csv".format(security_code=ID)
+        print(url_csvfile)
+        driver.get(url_csvfile)
+        time.sleep(np.random.rand() * 10 + 1)
+
+def main():
+    driver = init_driver()
+    login_yahoo(driver)
+    get_data(driver)
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
+
